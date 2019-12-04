a5e866d8701488b3eedf824e36f9a43fa6f06bb0
gleyvav2 <54995481+gleyvav2@users.noreply.github.com>
2019-12-04 15:30:50 -0800

    Add files via upload

--- s2.js
+++ s2.js
@@ -1,33 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('authenticate').addEventListener('click', function() {
-   chrome.identity.getAuthToken({interactive: true}, function(token) {
-     let init = {
-       method: 'GET',
-       async: true,
-       headers: {
-         Authorization: 'Bearer ' + token,
-         'Content-Type': 'application/json'
-       },
-         'contentType': 'json'
-     };
-
-     fetch(
-        'https://www.googleapis.com/drive/v3/files?corpora=user&&q=mimeType%3D%22application%2Fvnd.google-apps.document%22and%20trashed%3Dfalse&key=AIzaSyBM02Za0vpbuW8Kcim_xJpLVo4fzevX4m8',
-         init)
-         .then((response) => response.json())
-         .then(function(data) {
-         console.log(data)
-         value = data.files[0].id
-         console.log(value)
-         chrome.runtime.sendMessage({greeting: value}, function(response) {
-         });
-         })
-   });
- });
-});
-
-
-getmani = chrome.runtime.getManifest()
 loadScript('https://apis.google.com/js/api.js?onload=onApiLoad');
 function loadScript(url){
   var request = new XMLHttpRequest();
@@ -45,15 +15,8 @@ function loadScript(url){
 }
 
 var developerKey = "AIzaSyBM02Za0vpbuW8Kcim_xJpLVo4fzevX4m8";
-
-      // The Client ID obtained from the Google API Console. Replace with your own Client ID.
-      var clientId = "481015228871-q4rf7lfga87fu752pr2tnrgc49tmrv1h.apps.googleusercontent.com";
-
-      // Scope to use to access user's photos.
-      var scope = 'https://www.googleapis.com/auth/drive.file';
-
-
-
+var clientId = "481015228871-q4rf7lfga87fu752pr2tnrgc49tmrv1h.apps.googleusercontent.com";
+var scope = 'https://www.googleapis.com/auth/drive.file';
 var pickerApiLoaded = false;
 
 
@@ -63,8 +26,11 @@ function onApiLoad() {
   gapi.load('picker', onPickerApiLoad);
 }
 function onAuthApiLoad() {
-  var authBtn = document.getElementById('execute');  
+  var authBtn = document.getElementById('authenticate');  
   authBtn.addEventListener('click', function() {
+    document.getElementById('main').style.width="800px";
+    document.getElementById('main').style.height="600px";
+
     chrome.identity.getAuthToken({interactive: true}, function(token){ token
     var picker = new google.picker.PickerBuilder().
     addView(google.picker.ViewId.DOCUMENTS).
@@ -88,12 +54,11 @@ function handleAuthResult(authResult) {
     createPicker();
   }
 }
-
-// Create and render a Picker object for picking from Google Photos.
 function createPicker() {
   if (pickerApiLoaded && oauthToken) {
     var picker = new google.picker.PickerBuilder().
         addView(google.picker.ViewId.DOCUMENTS).
+        enableFeature(google.picker.Feature.SIMPLE_UPLOAD_ENABLED	).
         setOAuthToken(oauthToken).
         setDeveloperKey(developerKey).
         setCallback(pickerCallback).
@@ -101,15 +66,17 @@ function createPicker() {
     picker.setVisible(true);
   }
 }
-
-// A simple callback implementation.
 function pickerCallback(data) {
-  var url = 'nothing';
+  var url ;
   if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
     var doc = data[google.picker.Response.DOCUMENTS][0];
     url = doc[google.picker.Document.ID];
     chrome.storage.local.set({key: url}, function() {
-      console.log('Value is set to ' + url);
     })
+    window.close()
+  }
+  else if 
+  (data[google.picker.Response.ACTION] == google.picker.Action.CANCEL){
+    window.close()
   }
 }
