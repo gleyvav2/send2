1457d2395976fa77aa4f619e090968fd2be2a0a4
gleyvav2 <54995481+gleyvav2@users.noreply.github.com>
2019-12-04 21:35:44 -0800

    Add files via upload
    Added top var to define if the user wants to send the information to the top or bot of the doc

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
-var top = "on"
+let top1 = "on"
 chrome.contextMenus.create({
   "id":"sendtodoc",
   "title": "Send2",
   "contexts": ["selection"],
 })
+
+if (top1 == "on"){
 chrome.contextMenus.onClicked.addListener(function(info){
   info.selectionText
   chrome.storage.local.get(['key','key2'], function(result) {
@@ -50,4 +52,53 @@ chrome.contextMenus.onClicked.addListener(function(info){
             },
         function(){});
         chrome.notifications.clear('success', function (){}) });
-           })})})
+           })})})}
+
+else if (top1 =="off"){
+           chrome.contextMenus.onClicked.addListener(function(info){
+            info.selectionText
+            chrome.storage.local.get(['key','key2'], function(result) {
+              gg = result.key
+              title = result.key2
+            chrome.identity.getAuthToken({interactive: true}, function(token) {
+              let init = {
+                method: 'POST',
+                async: true,
+                headers: {
+                  Authorization: 'Bearer ' + token,
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  "requests": [
+                    {
+                      "insertText": {
+                        "endOfSegmentLocation": {
+                          "segmentId": ""
+                        },
+                        "text":"\n" +info.selectionText
+                      }
+                    }
+                  ]
+                }),
+                'contentType': 'json'
+              };
+          
+              fetch(
+                  "https://docs.googleapis.com/v1/documents/"+ gg +":batchUpdate?access_token=yes&key=AIzaSyBM02Za0vpbuW8Kcim_xJpLVo4fzevX4m8",
+                  init)
+                  .then((response) => response.json())
+                  .then(function(data) {
+                    console.log(data)
+                    chrome.notifications.clear('success', function (){})
+                    chrome.notifications.create(
+                      'success',{   
+                      type: 'basic', 
+                      iconUrl: 'send2.png', 
+                      title: "Send2", 
+                      message: "Your message has been sent to the following document: " +title ,
+                      silent:true,
+                      priority:0 
+                      },
+                  function(){});
+                  chrome.notifications.clear('success', function (){}) });
+                     })})})}
