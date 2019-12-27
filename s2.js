8165166df98b49d1ad7f61906e7447d6a4b8781d
gleyvav2 <54995481+gleyvav2@users.noreply.github.com>
2019-12-27 11:35:17 -0800

    Add files via upload
    Working on UI
    Added SMS tab with correct handlers.

--- s2.js
+++ s2.js
@@ -13,6 +13,40 @@ function loadScript(url){
 	request.open('GET', url);
 	request.send();
 }
+
+//Handles selected doc menu highlighter
+(function () {
+  var lastTitle = "send2 doc";
+  function checkTitle() {
+      if (lastTitle == document.title) {
+          document.addEventListener('DOMContentLoaded', function () {
+          docs = document.getElementById('docs')
+          docs.style.backgroundColor  = "#77aca2"
+          docs.style.color = "white"
+      })}
+     else if (lastTitle !== document.title) {
+      document.addEventListener('DOMContentLoaded', function () {
+      docs = document.getElementById('sms')
+      docs.style.backgroundColor  = "#77aca2"
+      docs.style.color = "white"
+  })
+    }
+  };
+  checkTitle();
+})();
+
+//Handles selected doc menu functions
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('docs').addEventListener('click', function() {
+    window.location = "/sendto.html";
+  })})
+
+  document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('sms').addEventListener('click', function() {
+      window.location = "/sendtosms.html";
+    })})
+
+
 var developerKey = "AIzaSyBM02Za0vpbuW8Kcim_xJpLVo4fzevX4m8";
 var clientId = "481015228871-q4rf7lfga87fu752pr2tnrgc49tmrv1h.apps.googleusercontent.com";
 var scope = 'https://www.googleapis.com/auth/drive.file';
@@ -124,47 +158,6 @@ document.addEventListener('DOMContentLoaded', function () {
         var newURL = "./tutorial/tutorial.html";
         chrome.tabs.create({ url: newURL });
       })})
-
-   /*/   Remove comment to add SMS
-document.addEventListener('DOMContentLoaded', function () {
-  var phone = document.getElementById('phone');  
-  phone.addEventListener('click', function() {
-    chrome.tabs.getSelected(null,function(tab) {
-    var tablink = tab.url;
-
-    var clientId = "AC7c0420605b10b3bc1ec2bafc071553d9";
-    var clientSecret = "4beb056670936962dcebc9e26db3500a";
-    var authorizationBasic = window.btoa(clientId + ':' + clientSecret);
-    
-    var request = new XMLHttpRequest();
-    request.open('POST',"https://api.twilio.com/2010-04-01/Accounts/AC7c0420605b10b3bc1ec2bafc071553d9/Messages.json" , true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.setRequestHeader('Authorization', 'Basic ' + authorizationBasic);
-    request.setRequestHeader('Accept', 'application/json');
-    request.send("Body="+tablink+"&From=+16192899915&To=+16197271386");
-    request.onreadystatechange = function () {    var checkrequest = request.status
-        if (checkrequest == 200){chrome.notifications.create(
-          'success',{   
-          type: 'basic', 
-          iconUrl: 'send2.png', 
-          title: "Send2", 
-          message: "Please Authorize the app or select a document" ,
-          silent:true,
-          priority:2 
-          })}
-          else if (checkrequest == 201){
-            chrome.notifications.clear('success', () => {
-            chrome.notifications.create(
-              'success',{   
-              type: 'basic', 
-              iconUrl: 'send2.png', 
-              title: "Send2", 
-              message: "Your message has been sent to the following number: "  ,
-              silent:true,
-              priority:0 
-            })})}}})})})
-           /*/             
-  
 
 
 document.addEventListener('DOMContentLoaded', function () {
---
+++ s2sms.js
@@ -0,0 +1,106 @@
+loadScript('https://apis.google.com/js/api.js?onload=onApiLoad');
+function loadScript(url){
+  var request = new XMLHttpRequest();
+	request.onreadystatechange = function(){
+		if(request.readyState !== 4) {
+			return;
+		}
+		if(request.status !== 200){
+			return;
+		}
+		eval(request.responseText);
+	};
+	request.open('GET', url);
+	request.send();
+}
+
+//Handles selected doc menu highlighter
+(function () {
+  var lastTitle = "send2 doc";
+  function checkTitle() {
+      if (lastTitle == document.title) {
+          document.addEventListener('DOMContentLoaded', function () {
+          docs = document.getElementById('docs')
+          docs.style.backgroundColor  = "#77aca2"
+          docs.style.color = "white"
+      })}
+     else if (lastTitle !== document.title) {
+      document.addEventListener('DOMContentLoaded', function () {
+      docs = document.getElementById('sms')
+      docs.style.backgroundColor  = "#77aca2"
+      docs.style.color = "white"
+  })
+    }
+  };
+  checkTitle();
+})();
+
+//Handles selected doc menu functions
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('docs').addEventListener('click', function() {
+    window.location = "/sendto.html";
+  })})
+
+  document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('sms').addEventListener('click', function() {
+      window.location = "/sendtosms.html";
+    })})
+
+
+var developerKey = "AIzaSyBM02Za0vpbuW8Kcim_xJpLVo4fzevX4m8";
+var clientId = "481015228871-q4rf7lfga87fu752pr2tnrgc49tmrv1h.apps.googleusercontent.com";
+var scope = 'https://www.googleapis.com/auth/drive.file';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+var donatebtn = document.getElementById('donate');  
+donatebtn.addEventListener('click', function() {
+    var newURL = "https://ko-fi.com/send22";
+    chrome.tabs.create({ url: newURL });
+  })})
+
+document.addEventListener('DOMContentLoaded', function () {
+    var donatebtn = document.getElementById('tutorial');  
+    donatebtn.addEventListener('click', function() {
+        var newURL = "./tutorial/tutorial.html";
+        chrome.tabs.create({ url: newURL });
+      })})
+
+//Send current page SMS
+document.addEventListener('DOMContentLoaded', function () {
+  var phone = document.getElementById('phone');  
+  phone.addEventListener('click', function() {
+    chrome.tabs.getSelected(null,function(tab) {
+    var tablink = tab.url;
+
+    var clientId = "AC7c0420605b10b3bc1ec2bafc071553d9";
+    var clientSecret = "4beb056670936962dcebc9e26db3500a";
+    var authorizationBasic = window.btoa(clientId + ':' + clientSecret);
+    
+    var request = new XMLHttpRequest();
+    request.open('POST',"https://api.twilio.com/2010-04-01/Accounts/AC7c0420605b10b3bc1ec2bafc071553d9/Messages.json" , true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.setRequestHeader('Authorization', 'Basic ' + authorizationBasic);
+    request.setRequestHeader('Accept', 'application/json');
+    request.send("Body="+tablink+"Sent using Send2"+"&From=+16192899915&To=+16197271386");
+    request.onreadystatechange = function () {    var checkrequest = request.status
+        if (checkrequest == 200){chrome.notifications.create(
+          'success',{   
+          type: 'basic', 
+          iconUrl: 'send2.png', 
+          title: "Send2", 
+          message: "Please Authorize the app or select a document" ,
+          silent:true,
+          priority:2 
+          })}
+          else if (checkrequest == 201){
+            chrome.notifications.clear('success', () => {
+            chrome.notifications.create(
+              'success',{   
+              type: 'basic', 
+              iconUrl: 'send2.png', 
+              title: "Send2", 
+              message: "Your message has been sent to the following number: "  ,
+              silent:true,
+              priority:0 
+            })})}}})})})  
